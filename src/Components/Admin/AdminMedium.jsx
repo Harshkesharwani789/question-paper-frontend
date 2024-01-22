3bfdc0fdbabbece962ed9bdab4faae02df4da32c
Parnets8 <parnetstech8@gmail.com>
2024-01-22 14:26:06 +0530

    forth day

--- src/Components/Admin/AdminMedium.jsx
+++ src/Components/Admin/AdminMedium.jsx
@@ -4,44 +4,215 @@ import { AiFillDelete, AiFillEye } from "react-icons/ai";
 import { BiSolidEdit } from "react-icons/bi";
 import { BsSearch } from "react-icons/bs";
 import "../Admin/Admin.css";
+import axios from "axios";
+import swal from "sweetalert";
 
 const AdminMedium = () => {
-  const [show, setShow] = useState();
-  const [show1, setShow1] = useState();
-  const [show2, setShow2] = useState();
+  const admin = JSON.parse(sessionStorage.getItem("admin"));
+  const token = sessionStorage.getItem("token");
+
+  const [show, setShow] = useState(false);
+  const [show1, setShow1] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
   const handleClose1 = () => setShow1(false);
   const handleShow1 = () => setShow1(true);
+
+  //Post method
+  const [mediumName, setmediumName] = useState("");
+
+  const Addmedium = async () => {
+    try {
+      const config = {
+        url: "/admin/addMedium",
+        baseURL: "http://localhost:8000/api",
+        method: "post",
+        headers: {
+          "content-type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        data:{
+          mediumName:mediumName,
+          authId:admin?._id
+        }
+      };
+      
+      let res = await axios (config);
+      if(res.status == 200){
+        return alert(res.data.success);
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  };
+
+  // const AddMedium = async () => {
+  //   try {
+  //     if(!mediumName) return  swal({
+  //       title: "oops!!",
+  //       text: "Please Enter the medium",
+  //       icon: "error",
+  //       dangerMode: true,
+  //     });
+  //     const config = {
+  //       url: "/admin/addMedium",
+  //       method: "post",
+  //       baseURL: "http://localhost:8000/api",
+  //       headers: {
+  //         "content-type": "application/json" ,
+  //         Authorization: `Bearer ${token}`,
+  //       },
+  //       data: {
+  //         mediumName: mediumName,
+  //         authId: admin?._id,
+  //       },
+  //     };
+  //     let res = await axios (config)
+  //       if (res.status == 200) {
+  //         swal({
+  //           title: "Success!",
+  //           text: res.data.success,
+  //           icon: "success",
+  //           dangerMode: true,
+  //         });
+  //         getAddMedium();
+  //         handleClose();
+  //       }
+
+  //   } catch (error) {
+  //     console.log(error);
+  //     swal({
+  //       title: "oops",
+  //       text: error.response.data.error,
+  //       icon: "error",
+  //       dangerMode: true,
+  //     });
+  //   }
+  // };
+  //get
+  const [Medium, setMedium] = useState([]);
+  const [nochangedata, setnochangedata] = useState([]);
+  const getAddMedium = async () => {
+    try {
+      let res = await axios.get("http://localhost:8000/api/admin/getAllMedium");
+      if (res.status == 200) {
+        setMedium(res.data.success);
+        setnochangedata(res.data.success);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  //edit
+  const EditAddMedium = async (e) => {
+    e.preventDefault();
+    try {
+      const config = {
+        url: "/admin/updateMedium",
+        method: "put",
+        baseURL: "http://localhost:8000/api",
+        headers: { "content-type": "application/json" },
+      };
+      await axios(config).then((res) => {
+        if (res.status == 200) {
+          swal({
+            title: "Yeahh!!",
+            text: "Successfully Deleted",
+            icon: "success",
+            dangerMode: true,
+          });
+        }
+      });
+    } catch (error) {
+      console.log(error);
+      swal({
+        title: "oops",
+        text: error.response.data.error,
+        icon: "error",
+        dangerMode: true,
+      });
+    }
+  };
+  //delete
+  const [Data, setData] = useState("");
+  const [show2, setShow2] = useState(false);
   const handleClose2 = () => setShow2(false);
-  const handleShow2 = () => setShow2(true);
+  const handleShow2 = (item) => {
+    setShow2(true);
+    setData(item);
+  };
+  const DeleteMedium = async () => {
+    try {
+      const config = {
+        url: "/admin/deleteMedium" + Data + "/" + admin?._id,
+        method: "delete",
+        baseURL: "http://localhost:8000/api",
+        headers: {
+          "content-type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      };
+      await axios(config).then((res) => {
+        if (res.status == 200) {
+          swal({
+            title: "Yeahh!!",
+            text: "Successfully Deleted",
+            icon: "success",
+            dangerMode: true,
+          });
+          getAddMedium();
+          handleClose2();
+        }
+      });
+    } catch (error) {
+      console.log(error);
+      swal({
+        title: "oops",
+        text: error.response.data.msg,
+        icon: "error",
+        dangerMode: true,
+      });
+    }
+  };
+
   //   Row Filter
   const [itempage, setItempage] = useState(5);
 
-  //   DateRange Filter
-  const [data, setData] = useState([]);
-  const [startDate, setstartDate] = useState("");
-  const [endDate, setendDate] = useState("");
-  const filterData = () => {
-    if (!startDate) return alert("Please select from date");
-    if (!endDate) return alert("Please select to date");
-    const filteredData = data.filter((item) => {
-      const itemDate = new Date(item?.createdAt);
-      const startDateObj = new Date(startDate);
-      const endDateObj = new Date(endDate);
-
-      return itemDate >= startDateObj && itemDate <= endDateObj;
-    });
-    setData([...filteredData]);
+  //search filter for about us
+  const [searchH, setSearchH] = useState("");
+  const handleFilterH = (e) => {
+    if (e.target.value != "") {
+      setSearchH(e.target.value);
+      const filterTableH = nochangedata.filter((o) =>
+        Object.keys(o).some((k) =>
+          String(o[k])?.toLowerCase().includes(e.target.value?.toLowerCase())
+        )
+      );
+      setMedium([...filterTableH]);
+    } else {
+      setSearchH(e.target.value);
+      setMedium([...nochangedata]);
+    }
   };
+  const [searchTermH, setSearchTermH] = useState("");
+  const searchedProductH = Medium.filter((item) => {
+    if (searchTermH.value === "") {
+      return item;
+    }
+    if (item?.EName?.toLowerCase().includes(searchTermH?.toLowerCase())) {
+      return item;
+    } else {
+      return console.log("not found");
+    }
+  });
   // Pagination
   const [pageNumber, setPageNumber] = useState(0);
   const productPerPage = 5;
   const visitedPage = pageNumber * productPerPage;
-  const displayPage = data.slice(visitedPage, visitedPage + productPerPage);
-  const pageCount = Math.ceil(data.length / productPerPage);
+  const displayPage = Medium.slice(visitedPage, visitedPage + productPerPage);
+  const pageCount = Math.ceil(Medium.length / productPerPage);
   return (
     <>
       <div className="col-lg-4 d-flex justify-content-center">
@@ -54,6 +225,7 @@ const AdminMedium = () => {
             class="form-control"
             placeholder="Search..."
             aria-describedby="basic-addon1"
+            onChange={handleFilterH}
           />
         </div>
       </div>
@@ -86,37 +258,39 @@ const AdminMedium = () => {
             </thead>
 
             <tbody>
-              <tr>
-                <td>1</td>
+              {Medium?.map((item, i) => {
+                return (
+                  <tr>
+                    <td>{i + 1}</td>
 
-                <td>
-                  <img src="" alt="" style={{ width: "75px" }} />
-                </td>
+                    <td>{item?.mediumName}</td>
 
-                <td>
-                  {" "}
-                  <div style={{ display: "flex", gap: "20px" }}>
-                    <div>
-                      <BiSolidEdit
-                        className="text-success"
-                        style={{ cursor: "pointer", fontSize: "20px" }}
-                        onClick={() => {
-                          handleShow1();
-                        }}
-                      />{" "}
-                    </div>
-                    <div>
-                      <AiFillDelete
-                        className="text-danger"
-                        style={{ cursor: "pointer", fontSize: "20px" }}
-                        onClick={() => {
-                          handleShow2();
-                        }}
-                      />{" "}
-                    </div>
-                  </div>
-                </td>
-              </tr>
+                    <td>
+                      {" "}
+                      <div style={{ display: "flex", gap: "20px" }}>
+                        <div>
+                          <BiSolidEdit
+                            className="text-success"
+                            style={{ cursor: "pointer", fontSize: "20px" }}
+                            onClick={() => {
+                              handleShow1(item);
+                            }}
+                          />{" "}
+                        </div>
+                        <div>
+                          <AiFillDelete
+                            className="text-danger"
+                            style={{ cursor: "pointer", fontSize: "20px" }}
+                            onClick={() => {
+                              handleShow2(item?._id);
+                            }}
+                          />{" "}
+                        </div>
+                      </div>
+                    </td>
+                  </tr>
+                );
+              })}
             </tbody>
           </Table>
         </div>
@@ -144,7 +318,7 @@ const AdminMedium = () => {
         </Pagination>
         {/* Add Package modal */}
         <Modal show={show} onHide={handleClose}>
-          <Modal.Header closeButton style={{ backgroundColor: "#26AAE0" }}>
+          <Modal.Header style={{ backgroundColor: "#26AAE0" }}>
             <Modal.Title style={{ color: "white" }}>Add Medium</Modal.Title>
           </Modal.Header>
           <Modal.Body>
@@ -155,6 +329,7 @@ const AdminMedium = () => {
                   type="text"
                   placeholder="Enter Medium"
                   className="vi_0"
+                  onChange={(e) => setmediumName(e.target.value)}
                 />
               </div>
             </div>
@@ -179,7 +354,13 @@ const AdminMedium = () => {
           </Modal.Body>
           <Modal.Footer>
             <div className="d-flex">
-              <Button className="mx-2" variant="primary">
+              <Button
+                className="mx-2"
+                variant="primary"
+                onClick={() => {
+                  Addmedium();
+                }}
+              >
                 Add
               </Button>
             </div>
@@ -204,6 +385,8 @@ const AdminMedium = () => {
                   type="text"
                   placeholder="Enter Medium"
                   className="vi_0"
+                  value={mediumName}
+                  onChange={(e) => setmediumName(e.target.value)}
                 />
               </div>
             </div>
@@ -230,7 +413,13 @@ const AdminMedium = () => {
             <Button variant="danger" onClick={handleClose1}>
               Close
             </Button>
-            <Button variant="primary" style={{ backgroundColor: "#FAFA33" }}>
+            <Button
+              variant="primary"
+              style={{ backgroundColor: "#FAFA33" }}
+              onClick={(e) => {
+                EditAddMedium();
+              }}
+            >
               Edit
             </Button>
           </Modal.Footer>
@@ -257,7 +446,14 @@ const AdminMedium = () => {
             <Button variant="btn btn-secondary" onClick={handleClose2}>
               Close
             </Button>
-            <Button variant="primary">Delete</Button>
+            <Button
+              variant="primary"
+              onClick={() => {
+                DeleteMedium();
+              }}
+            >
+              Delete
+            </Button>
           </Modal.Footer>
         </Modal>
       </div>
