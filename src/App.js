4763d05d7c75f3c0e55879471202b6814994f3d8
parnets19 <parnetstech19@gmail.com>
2024-01-29 14:42:43 +0530

    29-1-2024

--- src/App.js
+++ src/App.js
@@ -39,6 +39,7 @@ import SyllabusCopy from "./Components/SyllabusCopy/SyllabusCopy";
 import Weightagecontent from "./Components/Admin/Weightagecontent";
 import AdminChapter from "./Components/Admin/AdminChapter";
 import AdminQuestionPaper from "./Components/Admin/Adminquestionpaper";
+import AdminSyllabusCopy from "./Components/Admin/AdminSyllabusCopy";
 
 function App() {
   return (
@@ -333,6 +334,18 @@ function App() {
                 children={
                   <>
                     <AdminChapter />
+                  </>
+                }
+              />
+            }
+          />
+            <Route
+            path="/adminsyllabuscopy"
+            element={
+              <Main
+                children={
+                  <>
+                    <AdminSyllabusCopy />
                   </>
                 }
               />
