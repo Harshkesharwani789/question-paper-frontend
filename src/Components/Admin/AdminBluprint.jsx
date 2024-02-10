d2b2b3c837e0e1bd5d632ecf2806e25ab603f355
Parnets19 <parnetstech19@gmail.com>
2024-02-10 17:13:21 +0530

    am

--- src/Components/Admin/AdminBluprint.jsx
+++ src/Components/Admin/AdminBluprint.jsx
@@ -1802,7 +1802,7 @@ function AdminBlueprint() {
                                             </button>
                                           </div>
                                         </div> */}
-                                        <div className="col-md-12">
+                                        {/* <div className="col-md-12">
                                           <div className="do-sear mt-2">
                                             <Table
                                               responsive
@@ -1899,7 +1899,7 @@ function AdminBlueprint() {
                                               </tbody>
                                             </Table>
                                           </div>
-                                        </div>
+                                        </div> */}
                                       </div>
                                       <div className="row">
                                         <div className="col-md-4">
@@ -1971,14 +1971,19 @@ function AdminBlueprint() {
                                               );
                                             }}
                                           >
-                                            <option value="">
-                                              Selete Question Type
-                                            </option>
-                                            {view?.AllQType?.map((ele) => (
+                                       
+                                            <option value="">Select Question Types</option>
+                    <option value="M C">M.C (Multiple Choice)</option>
+                    <option value="V.S.A">V.S.A (Very Short Answer)</option>
+                    <option value="S.A">S.A (Short Answer)</option>
+                    <option value="L.A 1">L.A (Long Answer 1)</option>
+                    <option value="L.A 2">L.A (Long Answer 2)</option>
+                    <option value="L.A 3">L.A (Long Answer 3)</option>
+                                            {/* {view?.AllQType?.map((ele) => (
                                               <option value={ele?.QTyp}>
                                                 {ele?.QTyp}
                                               </option>
-                                            ))}
+                                            ))} */}
                                           </Form.Select>
                                         </div>
                                         <div className="col-md-4">
@@ -2063,9 +2068,20 @@ function AdminBlueprint() {
                                                   </tr>
                                                   )
                                                 })}
-                                               
+                                               <tr>
+                                               <td></td>
+                                                <td>
+                                                
+                                                </td>
+                                                <td></td>
+                                                <td></td>
+                                                <td>  Total Question:- {Arr5?.reduce((a,ele)=>a+Number(ele?.Blueprintnoofquestion),0)}</td>
+                                                <td>Total Marks:- {Arr5?.reduce((a,ele)=>a+Number(ele?.BluePrintmarksperquestion*ele?.Blueprintnoofquestion),0)}</td>
+                                                <td></td>
+                                               </tr>
                                               </tbody>
                                             </Table>
+                                          
                                           </div>
                                         </div>
                                       </div>
